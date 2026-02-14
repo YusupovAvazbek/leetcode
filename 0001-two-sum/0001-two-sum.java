7619df6f729c3ad497119b6864f1055fbb447250
YusupovAvazbek <120727489+YusupovAvazbek@users.noreply.github.com>
2026-02-14 20:47:48 +0500

    Time: 2 ms (99.17%), Space: 47.5 MB (9.17%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer,Integer> numsMap = new HashMap<>();
+        /*
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
                 if(nums[i] + nums[j] == target){
@@ -9,5 +10,14 @@ class Solution {
             }
         }
         return new int[]{0,0};
+        */
+        for(int i = 0; i < nums.length; i++){
+            
+            if(numsMap.containsKey(target - nums[i])){
+                return new int[]{numsMap.get(target - nums[i]),i};
+            }
+            numsMap.put(nums[i],i);
+        }
+        return new int[]{};
     }
 }
